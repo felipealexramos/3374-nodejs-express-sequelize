8a4629846587cc6965430fcf5ec6afbef78b0ac3
felipe <felipeadar108@gmail.com>
2025-01-07 12:22:46 -0300

    :sparkles: feat: criando relacionamentos entre tabelas

--- src/migrations/20250107145709-create-curso.js
+++ src/migrations/20250107145709-create-curso.js
@@ -18,6 +18,16 @@ module.exports = {
       data_inicio: {
         type: Sequelize.DATEONLY
       },
+      doscente_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: 'pessoas', key: 'id' } // Apesar do nome da propriedade ser model, o valor é o nome da tabela.
+      },
+      categoria_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: 'categorias', key: 'id' } 
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
